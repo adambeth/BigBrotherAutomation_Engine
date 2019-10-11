876a4f4fe08c1222e55ccc701cf5de9143198770
adambeth <adam.bethlehem1@gmail.com>
2019-10-11 13:09:18 +0200

    Added PWM spec configs

--- src/test/java/baseTest/testConfig.java
+++ src/test/java/baseTest/testConfig.java
@@ -10,34 +10,74 @@ import org.testng.annotations.BeforeClass;
 
 public class testConfig {
 
-    public static RequestSpecification financial_terms_controller;
-    public static RequestSpecification ussd_POST;
+    public static RequestSpecification PWM_ReserveFunds_RequestSpec;
+    public static RequestSpecification PWM_ReserveFunds_Behaviour_RequestSpec;
+    public static RequestSpecification PWM_Confirmration_RequestSpec;
+    public static RequestSpecification PWM_Confirmration_Behaviour_RequestSpec;
+    public static RequestSpecification PWM_Lookup_RequestSpec;
+    public static RequestSpecification PWM_Lookup_Behaviour_RequestSpec;
+
+
+
 
 
     @BeforeClass
 
     public void setUp() {
-        String className = this.getClass().getSimpleName();
 
-//        RestAssured.proxy("localhost",8888);
-        ussd_POST = new RequestSpecBuilder().
+
+        PWM_ReserveFunds_RequestSpec = new RequestSpecBuilder().
                 setBaseUri("http://docker-minion01.dev.za01.payd.co").
-                setPort(31905).
-                setBasePath("/trutequ/ussd/").
-                setContentType(ContentType.XML).
-
+                setPort(31915).
+                setBasePath("/fundingsource/reserve").
+                setContentType(ContentType.JSON).
+                addHeader("Content-Type","json").
                 build();
 
-        financial_terms_controller = new RequestSpecBuilder().
-                setBaseUri("http://docker-minion04.dev.za01.payd.co").
-                setPort(30309).
-                setBasePath("/financial-terms").
+        PWM_ReserveFunds_Behaviour_RequestSpec = new RequestSpecBuilder().
+                setBaseUri("http://docker-minion01.dev.za01.payd.co").
+                setPort(31914).
+                setBasePath("/api/FUNDING_SOURCE/249/apiBehaviour/EJ-Fc5kR9HemVCuM/set/reserve").
+                setContentType(ContentType.JSON).
+                addHeader("Content-Type","json").
+                build();
+
+        PWM_Confirmration_RequestSpec =  new RequestSpecBuilder().
+                setBaseUri("http://docker-minion01.dev.za01.payd.co").
+                setPort(31915).
+                setBasePath("/fundingsource/confirm").
+                setContentType(ContentType.JSON).
+                addHeader("Content-Type","json").
+                build();
+
+        PWM_Confirmration_Behaviour_RequestSpec = new RequestSpecBuilder().
+                setBaseUri("http://docker-minion01.dev.za01.payd.co").
+                setPort(31915).
+                setBasePath("/api/FUNDING_SOURCE/249/apiBehaviour/EJ-Fc5kR9HemVCuM/set/confirm").
+                setContentType(ContentType.JSON).
+                addHeader("Content-Type","json").
+                build();
+
+        PWM_Lookup_RequestSpec = new RequestSpecBuilder().
+                setBaseUri("http://docker-minion01.dev.za01.payd.co").
+                setPort(31914).
+                setBasePath("/api/FUNDING_SOURCE/249/apiTxnLookup/-aESIfw1mwedxvA-/lookup").
+                setContentType(ContentType.JSON).
+                addHeader("Content-Type","json").
+                build();
+
+        PWM_Lookup_Behaviour_RequestSpec = new RequestSpecBuilder().
+                setBaseUri("http://docker-minion01.dev.za01.payd.co").
+                setPort(31914).
+                setBasePath("/api/FUNDING_SOURCE/249/apiBehaviour/EJ-Fc5kR9HemVCuM/set/lookup").
+                setContentType(ContentType.JSON).
                 addHeader("Content-Type","json").
                 build();
 
 
-
     }
+
+
 
 
 }
