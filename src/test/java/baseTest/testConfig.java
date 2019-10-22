b877d0cb103a7ded7aedbd39606a78837777dcde
juan-claude.botha@payd.co <juan-claude.botha@clickatell.com>
2019-10-22 14:51:52 +0200

    Added maven profiling for QA endpoints

--- src/test/java/baseTest/testConfig.java
+++ src/test/java/baseTest/testConfig.java
@@ -8,6 +8,10 @@ import org.testng.ITestContext;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class testConfig {
     /*
     All .Control Request Specification
@@ -29,108 +33,108 @@ public class testConfig {
     public static RequestSpecification FM_GetClients_FUNDINGSOURCES;
 
     @BeforeClass
-
     public void setUp() {
+        Properties properties = loadPropertiesFile("config.properties");
 
         FM_GetClients_FUNDINGSOURCES = new RequestSpecBuilder()
-                .setBaseUri("http://control-ui-backend.qa.za01.payd.co")
-                .setBasePath("/management/dev/FUNDING_SOURCE")
+                .setBaseUri(properties.getProperty("FM_GetClients_FUNDINGSOURCES_BaseUri"))
+                .setBasePath(properties.getProperty("FM_GetClients_FUNDINGSOURCES_BasePath"))
                 .addHeader("Accept", "*/*")
                 .addHeader("Cache-Control", "no-cache")
-                .addHeader("Host", "control-ui-backend.qa.za01.payd.co")
+                .addHeader("Host", properties.getProperty("FM_GetClients_FUNDINGSOURCES_Host"))
                 .addHeader("Accept-Encoding", "Accept-Encoding")
                 .addHeader("Connection", "keep-alive").build();
 
         FM_GetClients_CLIENTS = new RequestSpecBuilder()
-                .setBaseUri("http://control-ui-backend.qa.za01.payd.co")
-                .setBasePath("/management/dev/CLIENT")
+                .setBaseUri(properties.getProperty("FM_GetClients_CLIENTS_BaseUri"))
+                .setBasePath(properties.getProperty("FM_GetClients_CLIENTS_BasePath"))
                 .addHeader("Accept", "*/*")
                 .addHeader("Cache-Control", "no-cache")
-                .addHeader("Host", "control-ui-backend.qa.za01.payd.co")
+                .addHeader("Host", properties.getProperty("FM_GetClients_CLIENTS_Host"))
                 .addHeader("Accept-Encoding", "Accept-Encoding")
                 .addHeader("Connection", "keep-alive").build();
 
         FM_GetClients_VENDORS = new RequestSpecBuilder()
-                .setBaseUri("http://control-ui-backend.qa.za01.payd.co")
-                .setBasePath("/management/dev/VENDOR")
+                .setBaseUri(properties.getProperty("FM_GetClients_VENDORS_BaseUri"))
+                .setBasePath(properties.getProperty("FM_GetClients_VENDORS_BasePath"))
                 .addHeader("Accept", "*/*")
                 .addHeader("Cache-Control", "no-cache")
-                .addHeader("Host", "control-ui-backend.qa.za01.payd.co")
+                .addHeader("Host", properties.getProperty("FM_GetClients_VENDORS_Host"))
                 .addHeader("Accept-Encoding", "Accept-Encoding")
                 .addHeader("Connection", "keep-alive").build();
 
 
 
         PWM_ReserveFunds_RequestSpec = new RequestSpecBuilder().
-                setBaseUri("http://docker-minion01.dev.za01.payd.co").
-                setPort(31915).
-                setBasePath("/fundingsource/reserve").
+                setBaseUri(properties.getProperty("PWM_ReserveFunds_RequestSpec_BaseUri")).
+                setPort(Integer.parseInt(properties.getProperty("PWM_ReserveFunds_RequestSpec_Port"))).
+                setBasePath(properties.getProperty("PWM_ReserveFunds_RequestSpec_BasePath")).
                 setContentType(ContentType.JSON).
                 addHeader("Content-Type", "json").
                 addHeader("fundingSourceId","249").
                 build().log().all();
 
         PWM_ReserveFunds_Behaviour_RequestSpec = new RequestSpecBuilder().
-                setBaseUri("http://docker-minion01.dev.za01.payd.co").
-                setPort(31914).
-                setBasePath("/api/FUNDING_SOURCE/249/apiBehaviour/EJ-Fc5kR9HemVCuM/set/reserve").
+                setBaseUri(properties.getProperty("PWM_ReserveFunds_Behaviour_RequestSpec_BaseUri")).
+                setPort(Integer.parseInt(properties.getProperty("PWM_ReserveFunds_Behaviour_RequestSpec_Port"))).
+                setBasePath(properties.getProperty("PWM_ReserveFunds_Behaviour_RequestSpec_BasePath")).
                 setContentType(ContentType.JSON).
                 addHeader("Content-Type", "json").
                 build();
 
         PWM_Confirmation_RequestSpec = new RequestSpecBuilder().
-                setBaseUri("http://docker-minion01.dev.za01.payd.co").
-                setPort(31915).
-                setBasePath("/fundingsource/confirm").
+                setBaseUri(properties.getProperty("PWM_Confirmation_RequestSpec_BaseUri")).
+                setPort(Integer.parseInt(properties.getProperty("PWM_Confirmation_RequestSpec_Port"))).
+                setBasePath(properties.getProperty("PWM_Confirmation_RequestSpec_BasePath")).
                 setContentType(ContentType.JSON).
                 addHeader("Content-Type", "json").
                 addHeader("fundingSourceId","249").
                 build();
 
         PWM_Confirmation_Behaviour_RequestSpec = new RequestSpecBuilder().
-                setBaseUri("http://docker-minion01.dev.za01.payd.co").
-                setPort(31915).
-                setBasePath("/api/FUNDING_SOURCE/249/apiBehaviour/EJ-Fc5kR9HemVCuM/set/confirm").
+                setBaseUri(properties.getProperty("PWM_Confirmation_Behaviour_RequestSpec_BaseUri")).
+                setPort(Integer.parseInt(properties.getProperty("PWM_Confirmation_Behaviour_RequestSpec_Port"))).
+                setBasePath(properties.getProperty("PWM_Confirmation_Behaviour_RequestSpec_BasePath")).
                 setContentType(ContentType.JSON).
                 addHeader("Content-Type", "json").
                 build();
 
         PWM_Lookup_RequestSpec = new RequestSpecBuilder().
-                setBaseUri("http://docker-minion01.dev.za01.payd.co").
-                setPort(31914).
-                setBasePath("/api/FUNDING_SOURCE/249/apiTxnLookup/-aESIfw1mwedxvA-/lookup").
+                setBaseUri(properties.getProperty("PWM_Lookup_RequestSpec_BaseUri")).
+                setPort(Integer.parseInt(properties.getProperty("PWM_Lookup_RequestSpec_Port"))).
+                setBasePath(properties.getProperty("PWM_Lookup_RequestSpec_BasePath")).
                 setContentType(ContentType.JSON).
                 addHeader("Content-Type", "json").
                 build();
 
         PWM_Lookup_Behaviour_RequestSpec = new RequestSpecBuilder().
-                setBaseUri("http://docker-minion01.dev.za01.payd.co").
-                setPort(31914).
-                setBasePath("/api/FUNDING_SOURCE/249/apiBehaviour/EJ-Fc5kR9HemVCuM/set/lookup").
+                setBaseUri(properties.getProperty("PWM_Lookup_Behaviour_RequestSpec_BaseUri")).
+                setPort(Integer.parseInt(properties.getProperty("PWM_Lookup_Behaviour_RequestSpec_Port"))).
+                setBasePath(properties.getProperty("PWM_Lookup_Behaviour_RequestSpec_BasePath")).
                 setContentType(ContentType.JSON).
                 addHeader("Content-Type", "json").
                 build();
 
         MWM_Ping_RequestSpec = new RequestSpecBuilder().
-                setBaseUri("http://docker-minion01.dev.za01.payd.co").
-                setPort(31910).
-                setBasePath("/vendor/ping").
+                setBaseUri(properties.getProperty("MWM_Ping_RequestSpec_BaseUri")).
+                setPort(Integer.parseInt(properties.getProperty("MWM_Ping_RequestSpec_Port"))).
+                setBasePath(properties.getProperty("MWM_Ping_RequestSpec_BasePath")).
                 setContentType(ContentType.JSON).
                 addHeader("Content-Type", "json").
                 build();
 
         MWM_Vend_RequestSpec = new RequestSpecBuilder().
-                setBaseUri("http://docker-minion01.dev.za01.payd.co").
-                setPort(31910).
-                setBasePath("/vendor/vend").
+                setBaseUri(properties.getProperty("MWM_Vend_RequestSpec_BaseUri")).
+                setPort(Integer.parseInt(properties.getProperty("MWM_Vend_RequestSpec_Port"))).
+                setBasePath(properties.getProperty("MWM_Vend_RequestSpec_BasePath")).
                 setContentType(ContentType.JSON).
                 addHeader("Content-Type", "json").
                 build();
 
         MWM_VendLookUp_RequestSpec = new RequestSpecBuilder().
-                setBaseUri("http://docker-minion01.dev.za01.payd.co").
-                setPort(31910).
-                setBasePath("/vendor/lookup").
+                setBaseUri(properties.getProperty("MWM_VendLookUp_RequestSpec_BaseUri")).
+                setPort(Integer.parseInt(properties.getProperty("MWM_VendLookUp_RequestSpec_Port"))).
+                setBasePath(properties.getProperty("MWM_VendLookUp_RequestSpec_BasePath")).
                 setContentType(ContentType.JSON).
                 addHeader("Content-Type", "json").
                 build();
@@ -138,7 +142,24 @@ public class testConfig {
 
     }
 
+    public static void main(String[] args) {
+        System.out.println(loadPropertiesFile("config.properties"));
+    }
 
+    public static Properties loadPropertiesFile(String filePath) {
+        Properties prop = new Properties();
+        try {
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            InputStream inputStream = classLoader.getResourceAsStream(filePath);
+//            InputStream resourceAsStream = testConfig.class.getClassLoader().getResourceAsStream(filePath);
+            prop.load(inputStream);
+        } catch (IOException e) {
+            System.err.println("Unable to load properties file : " + filePath);
+        }
+
+        return prop;
+
+    }
 
     @BeforeTest
     public void setExtent(ITestContext iTestContext) {
