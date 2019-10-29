3d967cfef7a920df8373aa764c6a2dbc301acf69
adambeth <adam.bethlehem1@gmail.com>
2019-10-29 07:46:32 +0200

    Made changes to foldet structure

---
+++ src/test/java/UI/TEST1.java
@@ -0,0 +1,17 @@
+package UI;
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import org.testng.annotations.Test;
+
+
+public class TEST1 {
+
+    @Test
+    public void test1(){
+
+//            open("https://www.google.com");
+        open("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java/3.141.59");
+    }
+}
